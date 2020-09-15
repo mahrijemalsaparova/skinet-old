550bb1ce216eae94b995c033acafc4d62784a9b2
mahri saparova <mahrijemal.saparova@gmail.com>
2020-09-15 13:32:35 +0300

    end of section 11

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { TestErrorComponent } from './core/test-error/test-error.component';
 import { HomeComponent } from './home/home.component';
 import { ProductDetailsComponent } from './shop/product-details/product-details.component';
-import { ShopComponent } from './shop/shop.component';
 
 const routes: Routes = [
   // adresin sonunda boşluk olursa HomeComponent sayfasına gidilir.
   {path: '', component: HomeComponent},
+  {path: 'test-error', component: TestErrorComponent},
+  {path: 'server-error', component: ServerErrorComponent},
+  {path: 'not-found', component: NotFoundComponent},
   // burada lazyloading yaptığımız için {path: 'shop', component: ShopComponent} yerine bu şekilde yazıyoruz.
   // çünkü shop route işlemimiz shop.module.ts'in kendi içinde gerçekleşecek app.module.ts'de değil.
   {path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule)},
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
+import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 
 
 @NgModule({
@@ -24,7 +25,11 @@ import { HomeModule } from './home/home.module';
    // ShopModule, gerek kalmadı çünkü kendi içinde routing yani lazyloading edilecek
     HomeModule
   ],
-  providers: [],
+  providers: [
+    // error.interceptor.ts kullanabilmek için
+    // muiti true olması demek bizim kendi interceptorumuz çalışmasın, app.modulun kendi interceptorları ile beraber çalışsın demek.
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
