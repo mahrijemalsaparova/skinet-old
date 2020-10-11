76a897fe2c0c9d94cb913d890ef4577fee2f5224
mahri saparova <mahrijemal.saparova@gmail.com>
2020-10-12 01:14:22 +0300

    async validation in client side

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,7 +1,9 @@
 import { Router } from '@angular/router';
 import { AccountService } from './../account.service';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AsyncValidatorFn, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { of, timer } from 'rxjs';
+import { switchMap, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-register',
@@ -21,7 +23,11 @@ export class RegisterComponent implements OnInit {
   createRegisterForm() {
     this.registerForm = this.fb.group({
       displayName: [null, [Validators.required]],
-      email: [null, [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
+      email: [null,
+        [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')],
+        // async validation : required ve pattern validasyonu pass ettikten sonra async validasyon çagırılır
+        [this.validateEmailNotTaken()]
+      ],
       password: [null, [Validators.required]]
     });
   }
@@ -34,5 +40,23 @@ export class RegisterComponent implements OnInit {
       console.log(error);
       this.errors = error.errors;
     });
+  }
+
+  validateEmailNotTaken(): AsyncValidatorFn {
+    return control => {
+      return timer(500).pipe(
+        switchMap(() => {
+          if (!control.value) {
+            return of(null);
+          }
+          // main job
+          return this.accountService.checkEmailExists(control.value).pipe(
+            map(res => {
+              return res ? {emailExists: true} : null;
+            })
+          );
+        })
+      );
+    };
   }
 }
